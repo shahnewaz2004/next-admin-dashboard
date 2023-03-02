bfdead3ab846cfc2fc24157d3b8ac3f7eb497165
Shhanewaz Rakib <personal.shahnewazrakib@gmail.com>
2023-03-02 19:31:04 +0600

    initial commit

---
+++ src/components/sidebar/components/Brand.tsx
@@ -0,0 +1,12 @@
+// Chakra imports
+import { Flex, useColorModeValue } from '@chakra-ui/react';
+import { HSeparator } from 'components/separator/Separator';
+
+export function SidebarBrand() {
+
+	return (
+		<h1 className=' text-center w-full text-2xl font-semibold pb-5 mb-10 border-b border-gray-70000'>Logo</h1>
+	);
+}
+
+export default SidebarBrand;
